f6b6601ade04a997314fc07107aaa61485cbcfbc
Paul Battley <pbattley@gmail.com>
2015-08-11 17:07:56 +0100

    Register Strategy subclasses

--- lib/lerna/strategy.rb
+++ lib/lerna/strategy.rb
@@ -1,5 +1,15 @@
 module Lerna
   class Strategy
+    def self.registry
+      @registry ||= {}
+    end
+
+    def self.inherited(subclass)
+      name = subclass.to_s.split(/::/).last
+      hyphenated = name.scan(/[A-Z][a-z_0-9]+/).map(&:downcase).join('-')
+      registry[hyphenated] = subclass
+    end
+
     def initialize(displays)
       @displays = displays
     end
---
+++ spec/strategy_spec.rb
@@ -0,0 +1,9 @@
+require 'lerna/strategy'
+require 'lerna/strategies/external_digital_only'
+
+RSpec.describe Lerna::Strategy do
+  it 'registers subclasses' do
+    expect(described_class.registry.fetch('external-digital-only')).
+      to eq(Lerna::Strategies::ExternalDigitalOnly)
+  end
+end
