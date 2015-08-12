a73d17664d155f12bff5a48a45153361b7c0efca
Paul Battley <pbattley@gmail.com>
2015-08-12 11:56:28 +0100

    Extract run to a tested class

---
+++ lib/lerna/runner.rb
@@ -0,0 +1,56 @@
+require 'lerna/strategy_selector'
+require 'lerna/state'
+
+module Lerna
+  class Runner
+    def initialize(logger:, strategies:, system:, state: State.new,
+                   strategy_selector: StrategySelector.new)
+      @logger = logger
+      @strategies = strategies
+      @system = system
+      @state = state
+      @strategy_selector = strategy_selector
+    end
+
+    def run
+      state.scan!
+      return unless state.changed?
+
+      log state_summary
+      strategy = find_strategy
+      if strategy
+        log "Using #{strategy.class}"
+        apply_strategy strategy
+      else
+        log 'No applicable strategy found'
+      end
+    end
+
+  private
+
+    attr_reader :state, :strategy_selector, :strategies
+
+    def log(s)
+      @logger.call(s)
+    end
+
+    def state_summary
+      state.displays.
+        map { |d| "#{d.name}#{d.connected? ? '*' : ''}" }.
+        join(' ')
+    end
+
+    def find_strategy
+      strategy_selector.call(strategies, state.displays)
+    end
+
+    def apply_strategy(strategy)
+      system 'xrandr', *strategy.configuration
+      system 'xset dpms force on'
+    end
+
+    def system(*args)
+      @system.call(*args)
+    end
+  end
+end
---
+++ spec/runner_spec.rb
@@ -0,0 +1,77 @@
+require 'lerna/runner'
+
+RSpec.describe Lerna::Runner do
+  subject {
+    described_class.new(
+      logger: logger,
+      system: system,
+      strategies: strategies,
+      state: state,
+      strategy_selector: strategy_selector
+    )
+  }
+  let(:logger) { double('logger', call: nil) }
+  let(:system) { double('system', call: nil) }
+  let(:strategies) { double('strategies') }
+  let(:strategy_selector) { double('strategy_selector', call: nil) }
+
+  after do
+    subject.run
+  end
+
+  context 'when the state has not changed' do
+    let(:state) { double('state', scan!: nil, changed?: false) }
+
+    it 'scans' do
+      expect(state).to receive(:scan!)
+    end
+
+    it 'does nothing' do
+      expect(system).not_to receive(:call)
+    end
+  end
+
+  context 'when the state has changed' do
+    let(:state) {
+      double('state', scan!: nil, changed?: true, displays: displays)
+    }
+    let(:displays) {
+      [double('display', name: 'ABC1', connected?: true)]
+    }
+
+    it 'scans' do
+      expect(state).to receive(:scan!)
+    end
+
+    it 'asks the strategy_selector for a strategy' do
+      expect(strategy_selector).to receive(:call).
+        with(strategies, displays).
+        and_return(nil)
+    end
+
+    context 'when a strategy is found' do
+      let(:strategy) {
+        double('strategy', configuration: %w[ --option --another ])
+      }
+      let(:strategy_selector) {
+        double('strategy_selector', call: strategy)
+      }
+
+      it 'calls xrandr with the strategy configuration' do
+        expect(system).to receive(:call).
+          with('xrandr', '--option', '--another')
+      end
+
+      it 'resets dpms' do
+        expect(system).to receive(:call).
+          with('xset dpms force on')
+      end
+    end
+
+    context 'when no strategy is found' do
+      it 'does nothing' do
+        expect(system).not_to receive(:call)
+      end
+    end
+  end
+end
